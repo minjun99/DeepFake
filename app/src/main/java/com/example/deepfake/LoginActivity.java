6b1e31fdcf57068b3de16e63f381eb2f3ae7f39e
minjun99 <krjun0315@kaist.ac.kr>
2020-10-26 21:57:54 +0900

    커밋

--- app/src/main/java/com/example/deepfake/LoginActivity.java
+++ app/src/main/java/com/example/deepfake/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.deepfake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = (Button)findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
